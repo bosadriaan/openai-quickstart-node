21d43556d03af5a2241ba6d205bd5b41d8e5f7a5
bosadriaan <78053487+bosadriaan@users.noreply.github.com>
2023-06-21 18:27:14 -0300

    before changing to user input

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,57 +6,61 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  if (!configuration.apiKey) {
-    res.status(500).json({
-      error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
-    });
-    return;
-  }
-
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
+  const { position, previousPosition, fieldSize, logoSize } = req.body;
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    const requestPayload = {
+      model: "gpt-3.5-turbo-0613",
+      messages: [
+        {
+          role: "system",
+          content: "You are an AI controlling a DVD logo moving across a screen. The position coordinates represents the top-left corner of the logo inside a field with a known height and width.",
+        },
+        {
+          role: "system",
+          content: "Step 1: Based on the current position and previous position, determine the direction and speed of the logo movement.",
+        },
+        {
+          role: "system",
+          content: "Step 2: Check if the logo is touching any of the 4 sides of the field. If it touches the left or right edge, make sure it bounces off the edge. If it touches the top or bottom edge, reverse the vertical direction. If it touches the left or right edge, reverse the horizontal direction.",
+        },
+        {
+          role: "system",
+          content: "Step 3: After these calculations, provide the updated position x and y. It will NEVER stay in the same place",
+        },
+        {
+          role: "user",
+          content: `Here are the current details: position - ${JSON.stringify(
+            position
+          )}, previous position - ${JSON.stringify(
+            previousPosition
+          )}, field size - ${JSON.stringify(
+            fieldSize
+          )}, logo size - ${JSON.stringify(logoSize)}.`,
+        },
+        {
+          role: "user",
+          content: `Now, please give the next position. Return the coordinates in the format: 'x:##, y:##. It will be parsed and used as the coordinates of the top left of the logo.`,
+        },
+      ],
+      
+    };
+
+    console.log("OpenAI API New Request:", JSON.stringify(requestPayload));
+
+    const response = await openai.createChatCompletion(requestPayload);
+
+    console.log("OpenAI API Response:", JSON.stringify(response.data));
+
+    const result = response.data.choices[0].message.content;
+
+    res.status(200).json({ result });
+  } catch (error) {
+    console.error(`Error with OpenAI API request: ${error.message}`);
+    res.status(500).json({
+      error: {
+        message: "An error occurred during your request.",
+      },
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
-    }
   }
-}
-
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [position, setPosition] = useState({ x: 100, y: 130 });
+  const [previousPosition, setpreviousPosition] = useState({ px: 95, py: 125 });
+  const fieldSize = { width: 200, height: 200 };
+  const logoSize = { width: 50, height: 50 };
+  const [aiResponse, setAiResponse] = useState("");
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -14,44 +17,78 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({
+          position,
+          previousPosition,
+          fieldSize,
+          logoSize,
+        }),
       });
 
       const data = await response.json();
+      console.log("Response data:", data); // Log the response data
+
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
+      // Parse the AI's response
+      setAiResponse(data.result);
+      const resultString = data.result;
+
+      
+      const px = position.x;
+      const py = position.y;
+      
+      const x = Number(resultString.match(/(?<=x: )\d+/)[0]);
+      const y = Number(resultString.match(/(?<=y: )\d+/)[0]);
+
+
+      console.log(x, y, px, py);
+
+      // Update the state
+      setpreviousPosition({ px, py });
+      setPosition({ x, y });
+      
+    } catch (error) {
       console.error(error);
       alert(error.message);
     }
   }
 
+  // Render the DVD logo at the current position
+  const logoStyle = {
+    position: "absolute",
+    left: `${position.x}px`,
+    top: `${position.y}px`,
+  };
+
+  // CSS styles for the field container
+  const fieldStyle = {
+    width: `${fieldSize.width}px`,
+    height: `${fieldSize.height}px`,
+    border: "4px solid black",
+    margin: "auto",
+    position: "relative",
+  };
+
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>AI DVD Logo</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
+        <h1>DVD Logo Bouncing with AI</h1>
+        <button onClick={onSubmit}>Move Logo</button>
+        <div className={styles.field} style={fieldStyle}>
+          <img src="/logo.png" style={logoStyle} />
+        </div>
+        <div className={styles.aiResponse}>
+          <p>AI Response:</p>
+          <p>{aiResponse}</p>
+        </div>
       </main>
     </div>
   );
