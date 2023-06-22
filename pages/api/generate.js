77f4e48b513a356740f146d3cbf6570ad57a75c0
bosadriaan <78053487+bosadriaan@users.noreply.github.com>
2023-06-21 22:39:40 -0300

    made it prettier

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,7 +6,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const { position, previousPosition, fieldSize, logoSize } = req.body;
+  const { position, previousPosition, fieldSize, logoSize, userPrompt } = req.body;
 
   try {
     const requestPayload = {
@@ -14,33 +14,11 @@ export default async function (req, res) {
       messages: [
         {
           role: "system",
-          content: "You are an AI controlling a DVD logo moving across a screen. The position coordinates represents the top-left corner of the logo inside a field with a known height and width.",
-        },
-        {
-          role: "system",
-          content: "Step 1: Based on the current position and previous position, determine the direction and speed of the logo movement.",
-        },
-        {
-          role: "system",
-          content: "Step 2: Check if the logo is touching any of the 4 sides of the field. If it touches the left or right edge, make sure it bounces off the edge. If it touches the top or bottom edge, reverse the vertical direction. If it touches the left or right edge, reverse the horizontal direction.",
-        },
-        {
-          role: "system",
-          content: "Step 3: After these calculations, provide the updated position x and y. It will NEVER stay in the same place",
+          content: "You are an AI model trained to process and respond to human language based on prompts provided to you.",
         },
         {
           role: "user",
-          content: `Here are the current details: position - ${JSON.stringify(
-            position
-          )}, previous position - ${JSON.stringify(
-            previousPosition
-          )}, field size - ${JSON.stringify(
-            fieldSize
-          )}, logo size - ${JSON.stringify(logoSize)}.`,
-        },
-        {
-          role: "user",
-          content: `Now, please give the next position. Return the coordinates in the format: 'x:##, y:##. It will be parsed and used as the coordinates of the top left of the logo.`,
+          content: userPrompt,
         },
       ],
       
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
   const [position, setPosition] = useState({ x: 100, y: 130 });
-  const [previousPosition, setpreviousPosition] = useState({ px: 95, py: 125 });
-  const fieldSize = { width: 200, height: 200 };
+  const [previousPosition, setPreviousPosition] = useState({ x: 90, y: 120 });
+  const fieldSize = { width: 300, height: 200 };
   const logoSize = { width: 50, height: 50 };
   const [aiResponse, setAiResponse] = useState("");
+  const [userExplanation, setUserExplanation] = useState("");
+  const [gameState, setGameState] = useState("");
+
+  useEffect(() => {
+    setGameState(
+  `Here are the current details: 
+    current position   x: ${position.x}, y: ${position.y}
+    previous position   x: ${previousPosition.x}, y: ${previousPosition.y}
+    field size  width: ${fieldSize.width}, height: ${fieldSize.height}
+    logo size  width: ${logoSize.width}, height: ${logoSize.height}
+  
+ 0, 0 is the top left of the screen. Determine the new position as: 'x:##, y:##.`
+    );
+  }, [position, previousPosition, fieldSize, logoSize]);
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -22,11 +36,11 @@ export default function Home() {
           previousPosition,
           fieldSize,
           logoSize,
+          userPrompt: userExplanation + gameState,
         }),
       });
 
       const data = await response.json();
-      console.log("Response data:", data); // Log the response data
 
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
@@ -35,20 +49,19 @@ export default function Home() {
       // Parse the AI's response
       setAiResponse(data.result);
       const resultString = data.result;
-
       
-      const px = position.x;
-      const py = position.y;
-      
-      const x = Number(resultString.match(/(?<=x: )\d+/)[0]);
-      const y = Number(resultString.match(/(?<=y: )\d+/)[0]);
-
-
-      console.log(x, y, px, py);
-
-      // Update the state
-      setpreviousPosition({ px, py });
-      setPosition({ x, y });
+      let matches = [...resultString.matchAll(/\bx:\s*(\d+),\s*y:\s*(\d+)\b/g)];
+      let lastMatch = matches[matches.length - 1];
+      if (lastMatch) {
+        let [newX, newY] = lastMatch.slice(1).map(Number);
+        
+        // Update the state
+        setPreviousPosition(position);
+        setPosition({
+          x: newX !== undefined ? newX : position.x,
+          y: newY !== undefined ? newY : position.y,
+        });
+      }
       
     } catch (error) {
       console.error(error);
@@ -56,18 +69,34 @@ export default function Home() {
     }
   }
 
-  // Render the DVD logo at the current position
+  // Render the DVD logo at the current and previous positions
   const logoStyle = {
     position: "absolute",
+    width: `${logoSize.width}px`,
+    height: `${logoSize.height}px`,
+  };
+
+  const previousLogoStyle = {
+    ...logoStyle,
+    left: `${previousPosition.x}px`,
+    top: `${previousPosition.y}px`,
+    opacity: 0.5, // Adjust the opacity for the faded effect
+    border: "1px solid black",
+  };
+
+  const currentLogoStyle = {
+    ...logoStyle,
     left: `${position.x}px`,
     top: `${position.y}px`,
+    zIndex: 1, // Ensure the current logo is on top
+    border: "1px solid black",
   };
 
   // CSS styles for the field container
   const fieldStyle = {
     width: `${fieldSize.width}px`,
     height: `${fieldSize.height}px`,
-    border: "4px solid black",
+    border: "2px solid black",
     margin: "auto",
     position: "relative",
   };
@@ -80,10 +109,20 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>DVD Logo Bouncing with AI</h1>
+        <h1>DVD screensaver by AI</h1>
+        <textarea 
+          value={userExplanation} 
+          onChange={e => setUserExplanation(e.target.value)} 
+          placeholder="Enter your explanation for the AI here"
+        />
+        <textarea 
+          value={gameState} 
+          readOnly
+        />
         <button onClick={onSubmit}>Move Logo</button>
         <div className={styles.field} style={fieldStyle}>
-          <img src="/logo.png" style={logoStyle} />
+          <img src="/logo.png" style={previousLogoStyle} />
+          <img src="/logo.png" style={currentLogoStyle} />
         </div>
         <div className={styles.aiResponse}>
           <p>AI Response:</p>
